b80765d6ba34694206e1378f225f63f27a953a98
cbrutofsky <cbrutofsky@users.noreply.github.com>
2019-11-09 16:58:11 -0500

    initial commit

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+const START_DISPLAY = "Hello, \
+              I'm Listenning!!!";
+OLED.init(128,95)
+
+/**
+ * Function that controls the message that the OLED is displaying.
+ * Default is set to the START_MESSAGE constant.
+ * If the message changes the screen is cleared before displaying the message.
+ * Parameters: String message
+ */
+function screenDisplay(default_display = START_DISPLAY, current_display: string): void {
+    if (current_display == default_display) {
+        OLED.writeString(current_display);
+    }
+    else if (default_display != current_display) {
+        if (default_display == START_DISPLAY) {
+            return
+        }
+        else {
+            OLED.clear()
+            OLED.writeString(current_display);
+        }
+    }
+}
+
+basic.forever(function () {
+
+})
